334a2a17df531cf2271278bd6bfa0bce3b6a29c3
Andrew Mackrodt <andrew@denimsoft.com>
2020-02-28 21:33:11 +0000

    Update ESP.qcow2 to FAT32 and remove empty clover partition

--- esp.sh
+++ esp.sh
@@ -11,7 +11,7 @@ loopback_cleanup() {
     set +e
 
     if [[ -n "$loop_device" ]]; then
-        sudo umount build/{EFI,clover} 2>/dev/null
+        sudo umount build/EFI 2>/dev/null
         sudo losetup -d "$loop_device"
     fi
 }
@@ -20,10 +20,10 @@ if [[ -d build/ ]]; then
     rm -rf build/
 fi
 
-mkdir -p build/{EFI,clover}
+mkdir -p build/EFI
 
 # create the empty image
-truncate -s 268435456 build/ESP.img
+truncate -s 260M build/ESP.img
 
 # detect the first free loopback device
 loop_device=$(losetup -f)
@@ -41,16 +41,10 @@ sed -e "s|/dev/nbd0|$loop_device|g" nbd0_sfdisk.txt | sudo sfdisk "$loop_device"
 sudo partprobe "$loop_device"
 
 # create the EFI filesystem
-sudo mkfs.fat -F16 -n"EFI" -h2048 -i"4679a914" -I "${loop_device}p1"
+sudo mkfs.fat -n"EFI" -i"4679a914" -I "${loop_device}p1"
 sudo mount "${loop_device}p1" build/EFI
 sudo rsync -rltD --exclude=.gitignore nbd0p1/ build/EFI/
 sudo umount build/EFI
-
-# create the clover filesystem
-sudo mkfs.fat -F16 -n"clover" -h202048 -i"4682bae0" -I "${loop_device}p2" # the source image had 202048 hidden sectors
-sudo mount "${loop_device}p2" build/clover
-sudo rsync -rltD --exclude=.gitignore nbd0p2/ build/clover/
-sudo umount build/clover
 
 # removing existing ESP.qcow2
 if [[ -f ../ESP.qcow2 ]]; then
@@ -62,5 +56,5 @@ qemu-img convert -f raw -O qcow2 build/ESP.img ../ESP.qcow2
 
 # clean up the build directory
 rm -f build/ESP.img
-rmdir build/{EFI,clover}
+rmdir build/EFI
 rmdir build/
